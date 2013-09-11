09daee80d20ae3c1826f617947d1a6c19fd243a7
Zear <zeartul@gmail.com>
2013-09-11 16:42:15 +0200

    Cleanup in SDLmain.c

--- src/SDLmain.c
+++ src/SDLmain.c
@@ -2,7 +2,7 @@
 #include <SDL.h>
 #include "global.h"
 
-SDL_Surface *screen = NULL;
+SDL_Surface *screen;
 SDL_Joystick *joy;
 
 int initSDL()
@@ -17,9 +17,14 @@ int initSDL()
 
 	joy = SDL_JoystickOpen(0);
 
-	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE);
 	SDL_ShowCursor(SDL_DISABLE);
-        SDL_WM_SetCaption("Wireless Connect 0.1", NULL);
+	screen = SDL_SetVideoMode(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_HWSURFACE | SDL_DOUBLEBUF);
+	if(screen == NULL)
+	{
+		fprintf(stderr, "ERROR (initSDL): Failed to set the screen surface.\n");
+		return 1;
+	}
+        SDL_WM_SetCaption("Wireless Connect", NULL);
 
 	return 0;
 }
